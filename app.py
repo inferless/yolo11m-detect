2ec32103c75b8a105a1830f72e94d0b51f3011ea
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2025-05-20 15:56:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,9 +24,9 @@ class ResponseObject(BaseModel):
 
 class InferlessPythonModel:
     @staticmethod
-    def download_file(url: str, save_path: str, overwrite: bool = False) -> bool:
+    def download_file(url: str, save_path: str) -> bool:
         os.makedirs(os.path.dirname(save_path), exist_ok=True)
-        if not overwrite and os.path.exists(save_path):
+        if os.path.exists(save_path):
             return True
         response = requests.get(url, stream=True, timeout=30)
         response.raise_for_status()
@@ -34,49 +34,34 @@ class InferlessPythonModel:
         block_size = 8192
         
         with open(save_path, 'wb') as f:
-            downloaded_size = 0
             for data in response.iter_content(block_size):
                 f.write(data)
-                downloaded_size += len(data)
-                if total_size > 0:
-                    done = int(50 * downloaded_size / total_size)
         return True
 
     def initialize(self, context=None):
         self.model_weights_url = "https://github.com/ultralytics/assets/releases/download/v8.3.0/yolo11m.pt"
         models_dir = os.path.join(os.getcwd(), "models_yolo")
         self.model_save_path = os.path.join(models_dir, "yolo11m.pt")
-        self.temp_image_dir = os.path.join(os.getcwd(), "temp_images")
 
-        if self.download_file(self.model_weights_url, self.model_save_path, overwrite=False):
-            self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        if self.download_file(self.model_weights_url, self.model_save_path):
             self.model = YOLO(self.model_save_path)
-            self.model.to(self.device) # Ensure model is on the correct device
-        os.makedirs(self.temp_image_dir, exist_ok=True)
+            self.model.to("cuda")
 
     def infer(self, request: RequestObjects) -> ResponseObject:
-        unique_filename = f"{uuid.uuid4()}.jpg"
-        downloaded_image_path = os.path.join(self.temp_image_dir, unique_filename)
-        self.download_file(request.image_url, downloaded_image_path, overwrite=True)
-
-        results = self.model.predict(
-            source=downloaded_image_path,
-            conf=request.confidence_threshold,
-            device=self.device)
-      
+        results = self.model.predict(source=request.image_url,conf=request.confidence_threshold,device="cuda")
         return_result = {"boxes": [], "confidences": [], "class_ids": [], "class_names": [], "annotated_image_base64_str": ""}
-        for result in results:
-            return_result["boxes"] = result.boxes.xyxyn.flatten().tolist()
-            return_result["confidences"] = result.boxes.conf.tolist()
-            return_result["class_ids"] = result.boxes.cls.tolist()
-            return_result["class_names"] = [result.names[int(cls_id)] for cls_id in return_result["class_ids"]]
+        
+        result = results[0]
+        return_result["boxes"] = result.boxes.xyxyn.flatten().tolist()
+        return_result["confidences"] = result.boxes.conf.tolist()
+        return_result["class_ids"] = result.boxes.cls.tolist()
+        return_result["class_names"] = [result.names[int(cls_id)] for cls_id in return_result["class_ids"]]
 
-            annotated_image_np = result.plot()
-            is_success, im_buf_arr = cv2.imencode(".jpg", annotated_image_np)
-            byte_im = im_buf_arr.tobytes()
-            return_result["annotated_image_base64_str"] = base64.b64encode(byte_im).decode('utf-8')
-          
-        os.remove(downloaded_image_path)  
+        annotated_image_np = result.plot()
+        is_success, im_buf_arr = cv2.imencode(".jpg", annotated_image_np)
+        byte_im = im_buf_arr.tobytes()
+        return_result["annotated_image_base64_str"] = base64.b64encode(byte_im).decode('utf-8')
+            
         return ResponseObject(**return_result)
       
     def finalize(self):
